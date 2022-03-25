d1c32eff700d331122a5325a2fa26e6f8086acac
Kylian Lavergne <inklay@outlook.fr>
2022-03-25 04:12:29 +0100

    [FIX] read access violation on Base_Box destructor

--- source/Save/Box/Box4.cpp
+++ source/Save/Box/Box4.cpp
@@ -2,7 +2,9 @@
 #include "../../utils/utils.hpp"
 
 void Box4::init_pkm(std::vector<char> data) {
-	for (int i = 0; i < 30; i++)
-		static_cast<Pk4&>(m_pokemons[i]).init(utils::slice(data, i * Pk4::size, (i + 1) * Pk4::size - 1));
+	for (int i = 0; i < 30; i++) {
+		Pk4 pkm;
+		pkm.init(utils::slice(data, i * Pk4::size, (i + 1) * Pk4::size - 1));
+		m_pokemons[i] = pkm;
+	}
 }
-
--- source/main.cpp
+++ source/main.cpp
@@ -3,8 +3,8 @@
 
 int main(int argc, char** argv) {
 	// Do not mind the hardcoded path, it's here to make debug easier
-	SavePL save = SavePL("C:\\Users\\inkla\\Desktop\\PL.SAV");
+	SavePL save = SavePL("C:\\Users\\inkla\\AppData\\Roaming\\Citra\\sdmc\\PL.SAV");
 	save.load();
-	std::cout << save.get_current_box().get_pokemon_at(0).get_sprite() << std::endl;
+	std::cout << save.get_current_box().get_name() << std::endl;
 	return 0;
 }
